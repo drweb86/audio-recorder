630f9febb8bec3f9f6dfadac39619ab729e23337
Siarhei Kuchuk <siarheik@outlook.com>
2022-01-21 21:01:26 +0300

    UWP ui stub

---
+++ src/HDE.AudioRecorder/AudioRecorder/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HDE.AudioRecorder
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NavigateToView("RecordingView");
+        }
+
+        private NavigationViewItem _lastItem;
+        private void NavigationView_OnItemInvoked(
+            Windows.UI.Xaml.Controls.NavigationView sender, 
+            NavigationViewItemInvokedEventArgs args)
+        {
+            var item = args.InvokedItemContainer as NavigationViewItem;
+            if (item == null || item == _lastItem)
+                return;
+            var clickedView = item.Tag?.ToString() ?? "SettingsView";
+            if (!NavigateToView(clickedView)) return;
+            _lastItem = item;
+        }
+
+        private bool NavigateToView(string clickedView)
+        {
+            var view = Assembly.GetExecutingAssembly()
+                .GetType($"HDE.AudioRecorder.Views.{clickedView}");
+
+            if (string.IsNullOrWhiteSpace(clickedView) || view == null)
+            {
+                return false;
+            }
+
+            ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
+            return true;
+        }
+
+        private void ContentFrame_OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new NavigationException(
+                $"Navigation failed {e.Exception.Message} for {e.SourcePageType.FullName}");
+        }
+
+        private void NavView_OnBackRequested(
+            Windows.UI.Xaml.Controls.NavigationView sender, 
+            NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentFrame.CanGoBack)
+                ContentFrame.GoBack();
+        }
+    }
+
+    internal class NavigationException : Exception
+    {
+        public NavigationException(string msg) : base(msg)
+        {
+            
+        }
+    }
+}
---
+++ src/HDE.AudioRecorder/AudioRecorder/Views/RecordingView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HDE.AudioRecorder.Views
+{
+    public sealed partial class RecordingView
+    {
+        public RecordingView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
